9387e3f0e9f1467f264cd78b2e481f026341aff6
Konstantin Bulanov <bulanov@netcracker.com>
2024-05-24 17:52:58 +0400

    111

--- esphome/components/modbus_server/modbus_server.cpp
+++ esphome/components/modbus_server/modbus_server.cpp
@@ -15,15 +15,20 @@ ModbusServer::ModbusServer() {}
 
 uint32_t ModbusServer::baudRate() { return this->parent_->get_baud_rate(); }
 
-void ModbusServer::setup() { mb.begin(this); }
+void ModbusServer::setup() {
+  uint16_t re_pin = 23;
+  uint16_t de_pin = 22;
+   mb.begin(this, de_pin, re_pin, true); }
+
+
 
 void ModbusServer::set_re_pin(GPIOPin *re_pin) {
-    if (re_pin != nullptr) {
-      re_pin_ = re_pin;
-      re_pin_->setup();
-      re_pin_->digital_write(LOW);
-      ESP_LOGD(TAG, "set_re_pin(): re_pin_ -> LOW");
-    }
+  if (re_pin != nullptr) {
+    re_pin_ = re_pin;
+    re_pin_->setup();
+    re_pin_->digital_write(LOW);
+    ESP_LOGD(TAG, "set_re_pin(): re_pin_ -> LOW");
+  }
 }
 
 void ModbusServer::set_de_pin(GPIOPin *de_pin) {
@@ -38,12 +43,12 @@ void ModbusServer::set_de_pin(GPIOPin *de_pin) {
 void ModbusServer::set_address(uint8_t address) { mb.slave(address); }
 
 bool ModbusServer::add_holding_register(uint16_t start_address, uint16_t value, uint16_t numregs) {
-          ESP_LOGD(TAG, "add Holding register");
+  ESP_LOGD(TAG, "add Holding register");
   return mb.addHreg(start_address, value, numregs);
 }
 
 bool ModbusServer::add_input_register(uint16_t start_address, uint16_t value, uint16_t numregs) {
-          ESP_LOGD(TAG, "add Input register");
+  ESP_LOGD(TAG, "add Input register");
   return mb.addIreg(start_address, value, numregs);
 }
 
@@ -77,13 +82,13 @@ void ModbusServer::on_write_input_register(uint16_t address, cbOnReadWrite cb, u
 
 // Stream class implementation:
 size_t ModbusServer::write(uint8_t data) {
-    if (( (re_pin_ != nullptr) || (de_pin_ != nullptr) ) && !sending) {
-        if (re_pin_ != nullptr)
-          re_pin_->digital_write(HIGH);
-          ESP_LOGV(TAG, "write(): re_pin_ -> HIGH");
-        if (de_pin_ != nullptr)
-          de_pin_->digital_write(HIGH);
-          ESP_LOGV(TAG, "write(): de_pin_ -> HIGH");
+  if (((re_pin_ != nullptr) || (de_pin_ != nullptr)) && !sending) {
+    if (re_pin_ != nullptr)
+      re_pin_->digital_write(HIGH);
+    ESP_LOGV(TAG, "write(): re_pin_ -> HIGH");
+    if (de_pin_ != nullptr)
+      de_pin_->digital_write(HIGH);
+    ESP_LOGV(TAG, "write(): de_pin_ -> HIGH");
     sending = true;
   }
   return uart::UARTDevice::write(data);
@@ -92,25 +97,21 @@ int ModbusServer::available() { return uart::UARTDevice::available(); }
 int ModbusServer::read() { return uart::UARTDevice::read(); }
 int ModbusServer::peek() { return uart::UARTDevice::peek(); }
 void ModbusServer::flush() {
-    uart::UARTDevice::flush();
-    if (( (re_pin_ != nullptr) || (de_pin_ != nullptr) ) && sending) {
-        if (re_pin_ != nullptr)
-          re_pin_->digital_write(LOW);
-          ESP_LOGV(TAG, "flush(): re_pin_ -> LOW");
-        if (de_pin_ != nullptr)
-          de_pin_->digital_write(LOW);
-          ESP_LOGV(TAG, "flush(): de_pin_ -> LOW");
+  uart::UARTDevice::flush();
+  if (((re_pin_ != nullptr) || (de_pin_ != nullptr)) && sending) {
+    if (re_pin_ != nullptr)
+      re_pin_->digital_write(LOW);
+    ESP_LOGV(TAG, "flush(): re_pin_ -> LOW");
+    if (de_pin_ != nullptr)
+      de_pin_->digital_write(LOW);
+    ESP_LOGV(TAG, "flush(): de_pin_ -> LOW");
     sending = false;
   }
 }
 
 void ModbusServer::loop() { mb.task(); };
 
-
-void ModbusServer::set_discovery_address(uint16_t address)
-{
-  
-}
+void ModbusServer::set_discovery_address(uint16_t address) {}
 
 }  // namespace modbus_server
 
